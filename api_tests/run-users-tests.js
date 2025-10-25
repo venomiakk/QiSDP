cbaf5fb3ce69ee19913a9909fcc35082f3cfaffe
Adrian Michałek <147884387+venomiakk@users.noreply.github.com>
2025-10-25 21:54:51 +0200

    added users tests and automation with ids

---
+++ api_tests/run-users-tests.js
@@ -0,0 +1,31 @@
+// 1. Zaimportuj bibliotekę Newman
+import { run } from "newman";
+
+console.log("Running API tests...");
+
+run(
+  {
+    collection: "./tool-shop-users-dev-tests.postman_collection.json",
+    environment: "./tool-shop-user-env.postman_environment.json",
+    reporters: "cli",
+  },
+  function (err, summary) {
+    if (err) {
+      console.error("Error running Newman:", err);
+      process.exit(1);
+    }
+
+    if (summary.run.failures.length > 0) {
+      console.error("❌ API Tests Failed!");
+      summary.run.failures.forEach(function (failure) {
+        console.log(
+          `[ERROR IN: "${failure.source.name}"] ${failure.error.message}`
+        );
+      });
+      process.exit(1);
+    } else {
+      console.log("✅ All API tests have passed!");
+      process.exit(0);
+    }
+  }
+);
